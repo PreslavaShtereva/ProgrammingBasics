f059e8d50725950afcc9ffdce6eaba616909dfcf
PreslavaShtereva <preslava.antonova03@gmail.com>
2023-03-10 22:59:48 +0100

    changes

---
+++ 06. Concatenate Data/06. Concatenate Data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string firstName;
+	string lastName;
+	int age;
+	string town;
+	cin >> firstName;
+	cin >> lastName;
+	cin >> age;
+	cin >> town;
+	cout << "You are " << firstName << " " << lastName << ", a " << age << "-years old person from " << town << ".";
+
+	return 0;
+}
+
+
