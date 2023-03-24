0604a2301b7018ee1797352280cab26cd4cdc822
PreslavaShtereva <127541105+PreslavaShtereva@users.noreply.github.com>
2023-03-24 21:09:46 +0100

    Create 03. Numbers 1...N with Step 3.cpp

---
+++ For Loop - Lab/03. Numbers 1...N with Step 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <=n; i+=3) {
+		cout << i<< "\n";
+	}
+
+return 0;
+}
+
+
