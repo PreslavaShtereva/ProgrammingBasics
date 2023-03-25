c3eecd2cd697cc8331d191707ec3a7da301b65c3
PreslavaShtereva <127541105+PreslavaShtereva@users.noreply.github.com>
2023-03-25 22:21:02 +0100

    Create 05. Character Sequence.cpp

---
+++ For Loop - Lab/05. Character Sequence.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	string n;
+	getline(cin,n);
+
+	for (int i = 0; i <=n.length()-1; i++) {
+		cout << n[i] << "\n";
+	}
+
+return 0;
+}
+
+
