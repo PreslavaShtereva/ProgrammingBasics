3fbc42829cbb4bbdcb20d89a418c425caf9dba86
PreslavaShtereva <127541105+PreslavaShtereva@users.noreply.github.com>
2023-03-25 22:20:27 +0100

    Create 04. Even Powers of 2.cpp

---
+++ For Loop - Lab/04. Even Powers of 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 0; i <=n; i+=2) {
+		cout << pow(2,i) << "\n";
+	}
+
+return 0;
+}
+
+
