72341ae6439bda0e8d03f6fe1c4553dce26ae8a4
PreslavaShtereva <127541105+PreslavaShtereva@users.noreply.github.com>
2023-03-27 23:46:26 +0200

    Create 01. Numbers Ending in 7.cpp

---
+++ For Loop - Exercise/01. Numbers Ending in 7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main() {
+	
+	for (int i = 0; i <= 1000; i++) {
+		if (i % 10 == 7) {
+			cout << i << '\n';
+		}
+	}
+
+return 0;
+}
